265bbc933e89c81a9babd93219d2daf6819ea0d2
Matthis Perreaux <maperrea@w-r5-p5.s19.be>
2020-01-28 12:16:24 +0100

    ifdef in .h

--- leak_detector.h
+++ leak_detector.h
@@ -6,23 +6,30 @@
 /*   By: maperrea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 15:28:48 by maperrea          #+#    #+#             */
-/*   Updated: 2020/01/22 19:37:57 by maperrea         ###   ########.fr       */
+/*   Updated: 2020/01/28 12:13:41 by maperrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEAK_DETECTOR_H
 # define LEAK_DETECTOR_H
 
-# include <unistd.h>
-# include <string.h>
-# include <stdio.h>
-# include <stdlib.h>
+# ifdef CHECK_LEAKS
+
+#  include <unistd.h>
+#  include <string.h>
+#  include <stdio.h>
+#  include <stdlib.h>
 
 void	*malloc_leak(size_t size, const char *file, int line);
 void	free_leak(void *ptr);
 void	print_leaks();
 
-# define malloc(X) malloc_leak(X, __FILE__, __LINE__)
-# define free(X)  free_leak(X)
+#  define malloc(X) malloc_leak(X, __FILE__, __LINE__)
+#  define free(X)  free_leak(X)
+#  define CHECK print_leaks();
+
+# else
+#  define CHECK
+# endif
 
 #endif
